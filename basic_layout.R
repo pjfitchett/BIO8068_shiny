cf9a4398f88c3234a80f284afed3c4d942a9e05f
Pj Fitchett <p.fitchett2@newcastle.ac.uk>
2021-04-30 11:19:38 +0100

    read in csv file, changed title, added histogram

--- basic_layout.R
+++ basic_layout.R
@@ -2,19 +2,23 @@
 
 library(shiny)
 
-# read in the image of the windfarm
-wfarm <- base64enc::dataURI(file="www/UKwindfarm.png", mime="image/png")
+# read in relevant data
+wfarm <- base64enc::dataURI(file="www/UKwindfarm.png", mime="image/png") # image
+habitats <- read.csv("www/habitats.csv")
 
 # Define UI ####
 ui <- fluidPage(
-  titlePanel("title panel"),
+  titlePanel("Windfarm in Cumbria"),
   
+  # Sidebar with buttons
   sidebarLayout(
   sidebarPanel("MySidebar",
                  h3("a button"),
+               # Submit button
                  actionButton(inputId="my_submitstatus",
                               label="Submit"),
-                 radioButtons(inputId = "habitat_checkbox", 
+               # Checkboxes - radioButtons
+                 radioButtons(inputId = "my_checkgroup", 
                                     h3("Select a habitat"), 
                                     choices = list("Woodland" = 1, 
                                                    "Grassland" = 2, 
@@ -22,19 +26,27 @@ ui <- fluidPage(
                                     selected = 1)
     ),  
   
+  # Main panel with text and images
   mainPanel(
     h1("Main heading"),
     h2("Subheading"),
     p("This will be to help planners assess potential windfarm", 
       "development areas in Cumbria, and achieve a ", strong("balance "),
       "between different", em("interest groups"), "and other users."),
-    img(src=wfarm)
+    img(src=wfarm),
+    plotOutput(outputId = "habitats_plot")
 )
 )
 )
 # Define server logic ####
+# Server takes info from buttons/sliders in UI and displays them
 server <- function(input, output) {
   
+  # creating a histogram
+  output$habitats_plot <- renderPlot(
+    # Output depends on which radioButton is selected
+    hist(habitats[, as.numeric(input$my_checkgroup)])
+  )
 }
 
 # Run the app ####
