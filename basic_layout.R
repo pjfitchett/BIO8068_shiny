a55b243c59ae3f872a4bdf58fdff0cfc91720c48
Pj Fitchett <p.fitchett2@newcastle.ac.uk>
2021-04-30 07:37:32 +0100

    Added some text, a button and an image

--- basic_layout.R
+++ basic_layout.R
@@ -2,6 +2,9 @@
 
 library(shiny)
 
+# read in the image of the windfarm
+wfarm <- base64enc::dataURI(file="www/UKwindfarm.png", mime="image/png")
+
 # Define UI ####
 ui <- fluidPage(
   titlePanel("title panel"),
@@ -12,7 +15,16 @@ ui <- fluidPage(
   ),
   mainPanel(
     h1("This is the main heading for my app"),
-    h2("here is a subheading")
+    h2("here is a subheading"),
+    p("This will be to help planners assess potential windfarm", 
+      "development areas in Cumbria, and achieve a ", strong("balance "),
+      "between different", em("interest groups"), "and other users."),
+    img(src=wfarm)
+),
+sidebarPanel("MySidebar",
+             h3("a button"),
+             actionButton(inputId="my_submitstatus",
+                          label="Submit")
 )
 )
 
