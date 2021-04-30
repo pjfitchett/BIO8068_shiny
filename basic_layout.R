8346a392343a28f5c4ef86f3b8322cadcfbb649f
Pj Fitchett <p.fitchett2@newcastle.ac.uk>
2021-04-30 10:33:11 +0100

    Changed position of the sidebar panel and added habitat checkboxes

--- basic_layout.R
+++ basic_layout.R
@@ -10,24 +10,28 @@ ui <- fluidPage(
   titlePanel("title panel"),
   
   sidebarLayout(
-    sidebarPanel("sidebar panel"),
-    mainPanel("main panel")
-  ),
+  sidebarPanel("MySidebar",
+                 h3("a button"),
+                 actionButton(inputId="my_submitstatus",
+                              label="Submit"),
+                 radioButtons(inputId = "habitat_checkbox", 
+                                    h3("Select a habitat"), 
+                                    choices = list("Woodland" = 1, 
+                                                   "Grassland" = 2, 
+                                                   "Urban" = 3),
+                                    selected = 1)
+    ),  
+  
   mainPanel(
-    h1("This is the main heading for my app"),
-    h2("here is a subheading"),
+    h1("Main heading"),
+    h2("Subheading"),
     p("This will be to help planners assess potential windfarm", 
       "development areas in Cumbria, and achieve a ", strong("balance "),
       "between different", em("interest groups"), "and other users."),
     img(src=wfarm)
-),
-sidebarPanel("MySidebar",
-             h3("a button"),
-             actionButton(inputId="my_submitstatus",
-                          label="Submit")
 )
 )
-
+)
 # Define server logic ####
 server <- function(input, output) {
   
